d36f68fdd30b977fde8a022524aecd6010cbd4f0
haolong.zhang <haolong.zhang@ck-telecom.com>
2015-10-28 14:40:28 +0800

    add wiki files

---
+++ unexeshell/wiki/from_internet/Android WatchDog分析 - hxdanya的专栏 - 博客频道 - CSDN.NET_files/ad-blog.js
@@ -0,0 +1,40 @@
+var ad_type = 'ms1365';
+var ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ad_type = 'ms1368_2';
+ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+
+document.writeln(ad_sc);
+
+
+ad_type = 'ms1371_3';
+ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+
+document.writeln(ad_sc);
+/*
+ad_type = 'ms1337_4';
+ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+
+document.writeln(ad_sc);
+
+ad_type = 'ms1324_5';
+ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+
+document.writeln(ad_sc);
+*/
