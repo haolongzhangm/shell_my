a7d0d813cc1df0aa1f0bb1a6d242c19777356363
haolong.zhang <haolong.zhang@ck-telecom.com>
2015-11-19 17:51:54 +0800

    1:update fsg command 2:add a try_code dir 3:add some RF file

---
+++ unexeshell/try_code/address_test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//define struct
+typedef struct {
+	int a;
+	int *b;
+} fisrt_int_typr, *p_first_int_type;
+
+typedef struct {
+	int c;
+	int *d;
+	fisrt_int_typr e_stuct;
+	p_first_int_type p_struct;
+} contont_struct, *contont_struct;
+
+
+//main 
+int main()
+{
+	fisrt_int_typr q;
+	fisrt_int_typr *p;
+
+	vmalloc()
+}
