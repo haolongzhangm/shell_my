2d0bde01ddf72ba60d890b4a1716a2c16d54145c
haolong.zhang <haolong.zhang@ck-telecom.com>
2016-12-13 17:54:53 +0800

    format qcom_flash.py

--- qcom_flash.py
+++ qcom_flash.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
-#this py will get/put data via ttyUSB*,which need root or $USER in dialout group(ttyUSB crw-rw---- 1 root dialout)
-#so there two ways to prepare env :
-#===============ways A===============================================================
-#with sudo qcom_flash.py
-#so you need put qcom_flash.py to /bin /usr/bin , which sudo command can find the env
-#may need sudo ln -s /home/zhl/shell_my/qcom_flash.py /bin/qcom_flash.py
-#may need sudo ln -s /home/zhl/shell_my/spl_qcom_download /bin/spl_qcom_download
-#may need sudo ln -s /home/zhl/shell_my/img_qcom_download /bin/img_qcom_download
-#=============================end ways A=============================================
-#===============ways B===============================================================
+#this py will get/put data via ttyUSB*,which need root or $USER in dialout
+#group caused by: (ttyUSB crw-rw---- 1 root dialout)
+#===============ways B====================================================
 #just add $USER into dialout group: sudo adduser $USER dialout
-#==============end ways B============================================================
-#recommendation ways B ==============================================================
 
-#ver 0.1 by haolong.zhang 2016/08/10
+#=========================================================================
+#base ver 0.1 by haolong.zhang 2016/08/10
+
+#-------------------------------------------------------------------------
+#update v1.0 2016/12/13
+#>>>Format char number to 80
+#>>>remove env prapre way A
+#>>>remove c style ;
+#-------------------------------------------------------------------------
+#=========================================================================
 
 import os
 import glob
@@ -23,6 +23,7 @@ import time
 parameter_list = ['withqcn', 'withoutqcn']
 sbl_name = 'null'
 def Usage():
+    print 'v1.0'
     print '[sudo] qcom_flash.py withqcn'
     print '[sudo] qcom_flash.py withoutqcn'
     exit()
@@ -37,16 +38,18 @@ def check_args():
         print parameter_list
         Usage()
 
-#we try to check the py exec pwd,only check the boot.img and emmc_appsboot.mbn etc
+#check the py exec pwd,only check the boot.img and emmc_appsboot.mbn etc
 def check_needed_file():
-    needed_file_set = ['NON-HLOS.bin', 'boot.img', 'emmc_appsboot.mbn', 'patch0.xml', 'rawprogram_unsparse_without_QCN.xml', 'rawprogram_unsparse.xml']
+    needed_file_set = ['NON-HLOS.bin', 'boot.img', 'emmc_appsboot.mbn', \
+                        'patch0.xml', 'rawprogram_unsparse_without_QCN.xml', \
+                        'rawprogram_unsparse.xml']
     file_list =os.listdir('./')
     #print file_list
     if set(needed_file_set).issubset(set(file_list)):
         print 'check needed file success'
     else:
         print 'check needed file failed , pls cd IMAGES_FOR_QMSCT fisrtly'
-        exit();
+        exit()
 
 #now we get the SBL file eg : prog_emmc_firehose_*
 def find_sbl_name():
@@ -61,18 +64,22 @@ def find_sbl_name():
 def download_img():
 #now we try to get dev/ttyUSB* all USBtty will be list at here
     if len(glob.glob('/dev/ttyUSB*')) ==0:
-        print 'can not find ttySUB dev, pls set phone into 9008 mode eg : adb reboot edl,exit now'
+        print 'can not find ttySUB dev, pls set phone into 9008 mode \
+                eg : adb reboot edl,exit now'
         exit()
-#now try flash SBL , because we have find some devs , may just serial dev ,so we need cap the flash result
+#now try flash SBL , because we have find some devs,
+#may just serial dev ,so we need cap the flash result
     for ttyUSB_dev in glob.glob('/dev/ttyUSB*'):
         print 'try to flash SBL via %s' % ttyUSB_dev
         print 'check %s access mode' % ttyUSB_dev
         if False == os.access(ttyUSB_dev, os.R_OK | os.W_OK):
             print 'Do not have access to W.R %s' % ttyUSB_dev
-            print 'pls use sudo qcom_flash.py or just add $USER into dialout group: sudo adduser $USER dialout'
+            print 'pls use sudo qcom_flash.py or just add $USER into \
+                    dialout group: sudo adduser $USER dialout'
             Usage()
 
-        #Formatt exe args;eg spl_qcom_download -p /dev/ttyUSB1 -s  13:prog_emmc_firehose_8976_ddr.mbn
+        #Formatt exe args;eg spl_qcom_download -p /dev/ttyUSB1 -s
+        #13:prog_emmc_firehose_8976_ddr.mbn
         exe_args = 'spl_qcom_download -p  %s  -s 13:%s' % (ttyUSB_dev, sbl_name)
         print exe_args
         SBL_flash_result = -1
@@ -90,9 +97,16 @@ def download_img():
     print 'sleep 3S'
     time.sleep(3)
 #now we try to flash all other img
-#Formatt exe args : eg img_qcom_download --port=/dev/ttyUSB1  --sendxml=patch0.xml --search_path=.  --sendxml=rawprogram_unsparse_without_QCN.xml --search_path=. --reset
-    withoutqcn_exc = 'img_qcom_download --port=%s  --sendxml=patch0.xml --search_path=.  --sendxml=rawprogram_unsparse_without_QCN.xml --search_path=. --reset' % ttyUSB_dev
-    withqcn_exc = 'img_qcom_download --port=%s  --sendxml=patch0.xml --search_path=.  --sendxml=rawprogram_unsparse.xml  --search_path=. --reset' % ttyUSB_dev
+#Formatt exe args : eg img_qcom_download --port=/dev/ttyUSB1
+#--sendxml=patch0.xml --search_path=.
+#--sendxml=rawprogram_unsparse_without_QCN.xml --search_path=. --reset
+    withoutqcn_exc = 'img_qcom_download --port=%s  --sendxml=patch0.xml \
+            --search_path=.  --sendxml=rawprogram_unsparse_without_QCN.xml \
+            --search_path=. --reset' % ttyUSB_dev
+
+    withqcn_exc = 'img_qcom_download --port=%s  --sendxml=patch0.xml \
+            --search_path=.  --sendxml=rawprogram_unsparse.xml \
+            --search_path=. --reset' % ttyUSB_dev
 
     if sys.argv[1] == parameter_list[0]:
         print 'Flash img withqcn'
@@ -119,4 +133,3 @@ check_args()
 check_needed_file()
 find_sbl_name()
 download_img()
-
