bee6a138f086354ad8fcf549452dcbc82116a29f
haolong.zhang <haolong.zhang@ck-telecom.com>
2017-08-14 20:57:41 +0800

    ua

--- unexeshell/learn_api/api.c
+++ unexeshell/learn_api/api.c
@@ -1,1 +1,9 @@
+dirty_writeback_interval // dirty writeback time
+wb_writeout_inc // wb write back and --
+bdi_set_max_ratio // bdi max ratio
+balance_dirty_pages_ratelimited // balance dirty memory state
+tag_pages_for_writeback // itag pages to be written by write_cache_pages
+write_cache_pages // walk the list of dirty pages of the given address space and write all of them.
+generic_writepages // walk the list of dirty pages of the given address space and writepage() all of them.
+
 
