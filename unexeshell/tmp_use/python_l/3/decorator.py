8f3939e509c64401adb5a82feaef0b5a53f99244
haolong.zhang <haolong.zhang@ck-telecom.com>
2017-08-25 15:05:34 +0800

    update py

---
+++ unexeshell/tmp_use/python_l/3/decorator.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import time
+
+#'''
+#step one
+def foo():
+    print('foo')
+    a = 0
+    for i in range(1, 1000):
+        a = a + i
+    print(a)
+
+
+def foo_2():
+    print('foo_2')
+#'''  
+
+
+'''
+#step two
+def foo():
+    start_time = time.time()
+    print('foo')
+    a = 0
+    for i in range(1, 1000):
+        a = a + i
+    print(a)
+
+    end_time = time.time()
+    print('foo use time %f ms' % ((end_time - start_time) * 10))
+
+
+def foo_2():
+    start_time = time.time()
+    print('foo_2')
+    end_time = time.time()
+    print('foo_2 use time %f ms' % ((end_time - start_time) * 10))
+
+
+'''
+
+
+#'''
+#step three
+def measure_time(func):
+    def wrapper():
+        start_time = time.time()
+        func()
+        end_time = time.time()
+        print('%s use time %f ms' % (func.__name__, (end_time - start_time) * 10))
+
+    return wrapper
+
+foo = measure_time(foo)
+foo_2 = measure_time(foo_2)
+#'''
+
+
+#step four
+
+
+foo()
+foo_2()
