2f9dcf68a9594f42fbfe140d6404c57f1e18ecd4
haolong.zhang <haolong.zhang@ck-telecom.com>
2018-08-24 09:10:09 +0800

    change to arm64

--- unexeshell/try_code/test_asan/build.sh
+++ unexeshell/try_code/test_asan/build.sh
@@ -3,6 +3,6 @@ clang++ -fsanitize=address -O0 -fno-omit-frame-pointer -g main.cc
 ./a.out
 
 echo "test android side======================================"
-arm-linux-androideabi-clang++ -fsanitize=address -O0 -fno-omit-frame-pointer -g -pie -fPIE main.cc
+aarch64-linux-android-clang++ -fsanitize=address -O0 -fno-omit-frame-pointer -g -pie -fPIE main.cc
 adb push a.out /system/bin/
 adb shell a.out
