67fd2f452672cd6d6f4a0f92e47b9d14b72810cf
zhanghaolong <zhanghaolong@megvii.com>
2019-07-24 15:04:27 +0800

    opt touchpad sensitive opt for macbookpro 2017 on ubuntu

--- macbookpro2017_touch_sensitive_opt.sh
+++ macbookpro2017_touch_sensitive_opt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # ref: https://help.ubuntu.com/community/SynapticsTouchpad
-sleep 5
+# ref: https://askubuntu.com/questions/483707/14-04-touchpad-is-too-sensitive
 xinput --set-prop 11 "Synaptics Finger" 100 160 600
+sleep 2
+xinput --set-prop 11 "Synaptics Noise Cancellation" 200 200
 echo "use command to check: xinput --watch-props 11"
