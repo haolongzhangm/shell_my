04caca732f6a80481437b08ee57df5ebd747fa81
zhanghaolong <zhanghaolong@megvii.com>
2019-12-20 15:05:00 +0800

    opt cl build

--- unexeshell/test_l_c/OpenCL_env/build_hello_world.sh
+++ unexeshell/test_l_c/OpenCL_env/build_hello_world.sh
@@ -1,5 +1,5 @@
 #from NDK17, default use clang
 echo "build for aarch64...."
-aarch64-linux-android-clang++ -pie -fPIE -IOpenCL -LOpenCL/aarch64_lib -lOpenCL -fuse-ld=gold src/HelloWorld.cpp -o out/HelloWorld_64
+aarch64-linux-android29-clang++ -static-libstdc++ -pie -fPIE -IOpenCL -LOpenCL/aarch64_lib -lOpenCL -fuse-ld=gold src/HelloWorld.cpp -o out/HelloWorld_64
 echo "build for arm...."
-arm-linux-androideabi-clang++ -pie -fPIE -IOpenCL -LOpenCL/arm_lib -lOpenCL -fuse-ld=gold src/HelloWorld.cpp -o out/HelloWorld_32
+armv7a-linux-androideabi23-clang++ -static-libstdc++ -pie -fPIE -IOpenCL -LOpenCL/arm_lib -lOpenCL -fuse-ld=gold src/HelloWorld.cpp -o out/HelloWorld_32
