77bffc4acf0564eb84600f08dfdec4bec2f70ba3
zhanghaolong <zhanghaolong@megvii.com>
2019-12-27 13:15:01 +0800

    update cl env

--- unexeshell/test_l_c/OpenCL_env/build.sh
+++ unexeshell/test_l_c/OpenCL_env/build.sh
@@ -1,5 +1,5 @@
 #from NDK17, default use clang
 echo "build for aarch64...."
-aarch64-linux-android-clang++ -pie -fPIE -IOpenCL -LOpenCL/aarch64_lib -lOpenCL -fuse-ld=gold src/OpenCLInfo.cpp -o out/OpenCLInfo_aarch64
+aarch64-linux-android29-clang++ -static-libstdc++ -pie -fPIE -IOpenCL -LOpenCL/aarch64_lib -lOpenCL -fuse-ld=gold src/OpenCLInfo.cpp -o out/OpenCLInfo_aarch64
 echo "build for arm...."
-arm-linux-androideabi-clang++ -pie -fPIE -IOpenCL -LOpenCL/arm_lib -lOpenCL -fuse-ld=gold src/OpenCLInfo.cpp -o out/OpenCLInfo_arm
+armv7a-linux-androideabi23-clang++ -static-libstdc++ -pie -fPIE -IOpenCL -LOpenCL/arm_lib -lOpenCL -fuse-ld=gold src/OpenCLInfo.cpp -o out/OpenCLInfo_arm
