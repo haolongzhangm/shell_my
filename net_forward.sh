ee3d82c8d80be59e485d5a0d58b5b39c00bf0121
zhanghaolong <zhanghaolong@jiigan.com>
2025-09-20 19:47:12 +0800

    add net foward

---
+++ net_forward.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+
+ssh -X -p 3222 -D 1080 -o "ServerAliveInterval 60" -f -N zhlvm@localhost
+
+export http_proxy="socks5://127.0.0.1:1080"
+export https_proxy="socks5://127.0.0.1:1080"
+
+curl --socks5 127.0.0.1:1080 http://ifconfig.me
+
+pkill -9 chrome || true
+google-chrome --proxy-server="socks5://127.0.0.1:1080"
