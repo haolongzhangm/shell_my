96d4c1102674339f6e87917b86dcb05bdb95b9a3
zhangyu09 <zhangyu09@jiigan.com>
2025-10-21 16:05:17 +0800

    support qcom new dispatch feature

--- unexeshell/test_l_c/OpenCL_env/build.sh
+++ unexeshell/test_l_c/OpenCL_env/build.sh
@@ -5,6 +5,8 @@ rm -rf out
 mkdir -p out
 
 echo "build for aarch64...."
-aarch64-linux-android29-clang++ -static-libstdc++ -pie -fPIE -Iopencl-stub/include/  src/OpenCLInfo.cpp opencl-stub/src/libopencl.cpp -llog -o out/OpenCLInfo_aarch64
+aarch64-linux-android29-clang++ -static-libstdc++ -pie -fPIE -Iopencl-stub/include/ -std=c++17 src/OpenCLInfo.cpp opencl-stub/src/libopencl.cpp -llog -o out/OpenCLInfo_aarch64
 echo "build for arm...."
-armv7a-linux-androideabi23-clang++ -static-libstdc++ -pie -fPIE -Iopencl-stub/include/  src/OpenCLInfo.cpp opencl-stub/src/libopencl.cpp -llog -o out/OpenCLInfo_arm
+armv7a-linux-androideabi23-clang++ -static-libstdc++ -pie -fPIE -Iopencl-stub/include/ -std=c++17 src/OpenCLInfo.cpp opencl-stub/src/libopencl.cpp -llog -o out/OpenCLInfo_arm
+
+llvm-strip out/OpenCLInfo_aarch64
